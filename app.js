7f09d69ea1a0a8459a685860ee6f6d81109aa6b2
Jacob Weldon <jacob.a.weldon@gmail.com>
2023-03-04 06:09:45 -0500

    setup for login

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://userAuth:test1234@cluster0.9j8r4ed.mongodb.net/user-auth?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {});
+router.get("/login", (req, res) => {});
+router.post("/login", (req, res) => {});
+
+module.exports = router;
